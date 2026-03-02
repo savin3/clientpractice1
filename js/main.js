a8a50b968a4f6aa90b9341e9af313c1e04cfd8c8
savin ilya <savin15ilya3112@gmail.com>
2026-03-03 00:34:39 +0700

    practical2

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ let app = new Vue({
         product: "Socks",
         description: 'A pair of warm, fuzzy socks',
         image: "./assets/vmSocks-blue-onWhite.jpg",
-        altText: "A pair of socks"
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
 
