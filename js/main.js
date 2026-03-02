e24ee023627dc4b969a2d825a0235d4310a4ddeb
savin ilya <savin15ilya3112@gmail.com>
2026-03-03 02:14:52 +0700

    added details in product

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
         inventory: 4,
-        onSale: true
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral']
     }
 })
 
