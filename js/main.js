a20d299468ef8f8e0a99d549feed12870766c5f1
savin ilya <savin15ilya3112@gmail.com>
2026-03-03 00:14:57 +0700

    added property and image and css

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: 'A pair of warm, fuzzy socks'
+        description: 'A pair of warm, fuzzy socks',
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks"
     }
 })
+
