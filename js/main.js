82806f9fc66d4ecdefd90db708ad1011a587af20
savin ilya <savin15ilya3112@gmail.com>
2026-03-03 22:10:31 +0700

    practical7

--- js/main.js
+++ js/main.js
@@ -48,6 +48,12 @@ let app = new Vue({
         },
         inStock(){
             return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale(){
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' saaale!'
+            }
+                return this.brand + ' ' + this.product + ' not sale :('
         }
     }
 })
